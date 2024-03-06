70537ebe040b91d6c9265263c4821f717aca6a96
SimoD91 <simone0091@hotmail.com>
2024-03-06 17:00:43 +0100

    Attributi classe videogioco

--- src/main/java/it/epicode/endgame/model/Videogioco.java
+++ src/main/java/it/epicode/endgame/model/Videogioco.java
@@ -1,4 +1,27 @@
 package it.epicode.endgame.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Videogioco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_videogioco")
+    private int idVideogioco;
+    private String titolo;
+    private int anno;
+    private String genere;
+    private LocalDate uscita;
+    private String teamDiSviluppo;
+    private String paese;
+    private int metascore;
+    private String plot;
+    private String poster;
+    private String immagini;
+    private String trailer;
+    private String recensione;
 }

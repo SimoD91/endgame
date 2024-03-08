079fbe8a681812f3a14947add26dcab02fbdb5c1
SimoD91 <simone0091@hotmail.com>
2024-03-09 00:53:27 +0100

    Preferiti request fix

--- src/main/java/it/epicode/endgame/dto/PreferitiRequest.java
+++ src/main/java/it/epicode/endgame/dto/PreferitiRequest.java
@@ -1,6 +1,10 @@
 package it.epicode.endgame.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PreferitiRequest {
+    @NotBlank(message = "Id utente obbligatorio")
     private int idUtente;
+    @NotBlank(message = "Id videogioco obbligatorio")
     private int idVideogioco;
 }

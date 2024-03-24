8a3951d25fa8ce1f4d299363552baaa58a4f751c
SimoD91 <simone0091@hotmail.com>
2024-03-25 00:08:43 +0100

    fix query videogioco repository

--- src/main/java/it/epicode/endgame/repository/VideogiocoRepository.java
+++ src/main/java/it/epicode/endgame/repository/VideogiocoRepository.java
@@ -15,10 +15,10 @@ public interface VideogiocoRepository extends JpaRepository<Videogioco, Integer>
     Page<Videogioco> findByUtente(Utente utente, Pageable pageable);
     @Query("SELECT v FROM Videogioco v ORDER BY v.titolo ASC")
     Page<Videogioco> findAllByOrderByTitoloAsc(Pageable pageable);
-    @Query("SELECT v FROM Videogioco v WHERE LOWER(v.titolo) LIKE %:titolo% ORDER BY v.titolo ASC")
+    @Query("SELECT v FROM Videogioco v WHERE v.titolo ILIKE concat('%', :titolo, '%') ORDER BY v.titolo ASC")
     Page<Videogioco> findByTitoloContainingIgnoreCase(String titolo, Pageable pageable);
     Page<Videogioco> findByAnnoDiUscita(int annoDiUscita, Pageable pageable);
-    @Query("SELECT v FROM Videogioco v WHERE lower(v.genere) LIKE lower(concat('%', :genere, '%'))")
+    @Query("SELECT v FROM Videogioco v WHERE v.genere ILIKE concat('%', :genere, '%')")
     Page<Videogioco> findByGenere(String genere, Pageable pageable);
     @Query("SELECT v FROM Videogioco v WHERE lower(v.console) LIKE lower(concat('%', :console, '%'))")
     Page<Videogioco> findByConsole(String console, Pageable pageable);
